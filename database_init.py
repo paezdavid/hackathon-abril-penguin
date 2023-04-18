9de86b29799ef2adad80482c45f5da371b4d02fc
Cheloamarilla <amarillamarcelo0@gmail.com>
2023-04-18 16:41:12 +0200

    agregue columna foto

--- database_init.py
+++ database_init.py
@@ -15,19 +15,24 @@ cursor.execute('''
      capacidad INTEGER, 
      ciudad TEXT,
      pais TEXT,
-     cant_camas INTEGER
+     cant_camas INTEGER,
+     foto TEXT
      ) ''')
 
+# with open('tacumbu.jpg','rb') as f:
+#     foto_tacumbu= f.read()
 
-cursor.execute(''' INSERT INTO penitenciarias (nombre ,cant_reclusos, direccion, cant_pabellones, cant_funcionarios, cant_celdas, capacidad, ciudad, pais,cant_camas) VALUES (?,?,?,?,?,?,?,?,?,?)''', ('Penitenciaria TACUMBÚ', 4000, 'AVDA SANTA TERESA', 2000, 600, 800, 3000, 'Asuncion', 'Paraguay', 7) )
+cursor.execute(''' INSERT INTO penitenciarias (nombre ,cant_reclusos, direccion, cant_pabellones, cant_funcionarios, cant_celdas, capacidad, ciudad, pais,cant_camas,foto) VALUES (?,?,?,?,?,?,?,?,?,?,?)''', ('Penitenciaria TACUMBÚ', 4000, 'AVDA SANTA TERESA', 2000, 600, 800, 3000, 'Asuncion', 'Paraguay', 7,'https://media.ultimahora.com/p/651b5a248dd33f0a09d4ac6c6a4fa764/adjuntos/161/imagenes/009/817/0009817748/carcel-el-buen-pastor-se-encuentra-una-zona-cotizada-asuncion.jpg') )
 
-cursor.execute(''' INSERT INTO penitenciarias (nombre ,cant_reclusos, direccion, cant_pabellones, cant_funcionarios, cant_celdas, capacidad, ciudad, pais,cant_camas) VALUES (?,?,?,?,?,?,?,?,?,?)''', ('carcel2', 4000, 'AVDA SANTA TERESA', 2000, 600, 800, 3000, 'Asuncion', 'Paraguay', 7) )
+#FALTA AGG BIEN LAS FOTOS
 
-cursor.execute(''' INSERT INTO penitenciarias (nombre ,cant_reclusos, direccion, cant_pabellones, cant_funcionarios, cant_celdas, capacidad, ciudad, pais,cant_camas) VALUES (?,?,?,?,?,?,?,?,?,?)''', ('carcel2', 4000, 'AVDA SANTA TERESA', 2000, 600, 800, 3000, 'Asuncion', 'Paraguay', 7) )
+# cursor.execute(''' INSERT INTO penitenciarias (nombre ,cant_reclusos, direccion, cant_pabellones, cant_funcionarios, cant_celdas, capacidad, ciudad, pais,cant_camas,foto) VALUES (?,?,?,?,?,?,?,?,?,?)''', ('carcel2', 4000, 'AVDA SANTA TERESA', 2000, 600, 800, 3000, 'Asuncion', 'Paraguay', 7) )
 
-cursor.execute(''' INSERT INTO penitenciarias (nombre ,cant_reclusos, direccion, cant_pabellones, cant_funcionarios, cant_celdas, capacidad, ciudad, pais,cant_camas) VALUES (?,?,?,?,?,?,?,?,?,?)''', ('carcel2', 4000, 'AVDA SANTA TERESA', 2000, 600, 800, 3000, 'Asuncion', 'Paraguay', 7) )
+# cursor.execute(''' INSERT INTO penitenciarias (nombre ,cant_reclusos, direccion, cant_pabellones, cant_funcionarios, cant_celdas, capacidad, ciudad, pais,cant_camas,foto) VALUES (?,?,?,?,?,?,?,?,?,?)''', ('carcel2', 4000, 'AVDA SANTA TERESA', 2000, 600, 800, 3000, 'Asuncion', 'Paraguay', 7) )
 
-cursor.execute(''' INSERT INTO penitenciarias (nombre ,cant_reclusos, direccion, cant_pabellones, cant_funcionarios, cant_celdas, capacidad, ciudad, pais,cant_camas) VALUES (?,?,?,?,?,?,?,?,?,?)''', ('carcel2', 4000, 'AVDA SANTA TERESA', 2000, 600, 800, 3000, 'Asuncion', 'Paraguay', 7) )
+# cursor.execute(''' INSERT INTO penitenciarias (nombre ,cant_reclusos, direccion, cant_pabellones, cant_funcionarios, cant_celdas, capacidad, ciudad, pais,cant_camas) VALUES (?,?,?,?,?,?,?,?,?,?)''', ('carcel2', 4000, 'AVDA SANTA TERESA', 2000, 600, 800, 3000, 'Asuncion', 'Paraguay', 7) )
+
+# cursor.execute(''' INSERT INTO penitenciarias (nombre ,cant_reclusos, direccion, cant_pabellones, cant_funcionarios, cant_celdas, capacidad, ciudad, pais,cant_camas) VALUES (?,?,?,?,?,?,?,?,?,?)''', ('carcel2', 4000, 'AVDA SANTA TERESA', 2000, 600, 800, 3000, 'Asuncion', 'Paraguay', 7) )
 
 
 # cursor.execute(''' DELETE FROM penitenciarias WHERE id= 1  ''') POR SI HAY QUE BORRAR ALGO MAS 
